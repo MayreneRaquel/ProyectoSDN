3151a5a89bce697cd65fccf6309d9686803ddfb9
Patrick <mayreneanchia08@gmail.com>
2018-11-18 18:10:51 -0600

    Estilitos.

--- src/Principal.js
+++ src/Principal.js
@@ -1,15 +1,10 @@
 import React, {Component} from 'react';
 import {datosJson} from './resources/datos.json'
-<<<<<<< HEAD
 import './CSS/Principal.css'
-=======
-import './CSS/Tabla.css'
->>>>>>> 760955ba665f5a3406e15693d8e9f01d04993c39
 import 'bootstrap/dist/css/bootstrap.css';
 
 class Principal extends Component {
 
-<<<<<<< HEAD
     constructor() {
         super();
         this.leerDatosMonitor = this.leerDatosMonitor.bind(this);
@@ -19,28 +14,6 @@ class Principal extends Component {
         };   
 	}
 
-=======
-    
-    constructor() {
-        super();
-        this.leerDatosMonitor = this.leerDatosMonitor.bind(this);
-
-        this.state = {
-            datosJ : null,
-        };
-	}
-
-    /* 
-    https://carlosazaustre.es/consumiendo-un-api-rest-desde-react-js-con-ecmascript6/ 
-    https://davidwalsh.name/fetch 
-    https://www.taniarascia.com/how-to-use-the-javascript-fetch-api-to-get-json-data/
-    https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    https://www.robinwieruch.de/react-fetching-data/
-    https://stackoverflow.com/questions/47417217/where-to-declare-variable-in-react-js
-    https://addons.mozilla.org/en-US/firefox/addon/react-devtools/     
-    */
-
->>>>>>> 760955ba665f5a3406e15693d8e9f01d04993c39
     componentDidMount(){
         this.leerDatosMonitor();        
     }
@@ -58,25 +31,17 @@ class Principal extends Component {
         }).catch(err => {
             // Do something for an error here
             console.error('Error:', err);
-<<<<<<< HEAD
         });         
-=======
-        }); 
->>>>>>> 760955ba665f5a3406e15693d8e9f01d04993c39
     }
 
     render() {
         return(
             <div>
-<<<<<<< HEAD
                 <link href="CSS/Principal.css" rel="stylesheet" type="text/css"/>
-=======
->>>>>>> 760955ba665f5a3406e15693d8e9f01d04993c39
                 <div className="jumbotron text-center">
                     <h1>Visualizador de trafico de red</h1>
                 </div>
 
-<<<<<<< HEAD
                 <div>
                     <nav className="navbar visible-xs">
                         <div className="container-fluid">
@@ -157,21 +122,6 @@ class Principal extends Component {
                                     </div>
                                 </div>
                             </div>
-=======
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-4">
-                            <h3>Column 1</h3>
-                            <p>Lorem ipsum dolor..</p>
-                        </div>
-                        <div className="col-sm-4">
-                            <h3>Column 2</h3>
-                            <p>Lorem ipsum dolor..</p>
-                        </div>
-                        <div className="col-sm-4">
-                            <h3>Column 3</h3>
-                            <p>Lorem ipsum dolor..</p>
->>>>>>> 760955ba665f5a3406e15693d8e9f01d04993c39
                         </div>
                     </div>
                 </div>
